cda07f7530d5fa048969978d57966bb0565dbe5e
Jhonatan <gestao@etsdigital.com.br>
2024-09-17 18:30:54 -0300

    Criação dos Repository Patterns

---
+++ TheatricalPlayersRefactoringKata.Domain/Repositories/IPlayRepository.cs
@@ -0,0 +1,9 @@
+﻿using TheatricalPlayersRefactoringKata.Domain.Entities;
+
+namespace TheatricalPlayersRefactoringKata.Domain.Repositories
+{
+    public interface IPlayRepository : IRepository<PlayEntity>
+    {
+        Task<PlayEntity> GetPlayBySlugAsync(string name);
+    }
+}
