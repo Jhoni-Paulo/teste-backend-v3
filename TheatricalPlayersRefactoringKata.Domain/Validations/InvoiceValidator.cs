5d29166485bcacf83978b3d0584223dbf2b070c3
Jhonatan <gestao@etsdigital.com.br>
2024-09-17 19:06:32 -0300

    Adicionando valaidações base de Entidade

---
+++ TheatricalPlayersRefactoringKata.Domain/Validations/InvoiceValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using TheatricalPlayersRefactoringKata.Domain.Entities;
+
+namespace TheatricalPlayersRefactoringKata.Domain.Validations
+{
+    public class InvoiceValidator : AbstractValidator<InvoiceEntity>
+    {
+        public InvoiceValidator()
+        {
+            RuleFor(x => x.Customer)
+                .NotEmpty().WithMessage("Customer is required.");
+
+            RuleFor(x => x.Performances)
+                .NotNull().WithMessage("Performances list cannot be null.")
+                .Must(p => p != null && p.Count > 0).WithMessage("Invoice must contain at least one performance.");
+        }
+    }
+}
---
+++ TheatricalPlayersRefactoringKata.Domain/Validations/PlayValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using TheatricalPlayersRefactoringKata.Domain.Entities;
+
+namespace TheatricalPlayersRefactoringKata.Domain.Validations
+{
+    public class PlayValidator : AbstractValidator<PlayEntity>
+    {
+        public PlayValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name must be less than 100 characters.");
+
+            RuleFor(x => x.Lines)
+                .GreaterThan(0).WithMessage("Lines must be at least 1.");
+
+            RuleFor(x => x.Type)
+                .IsInEnum().WithMessage("Invalid play type.");
+        }
+    }
+}
